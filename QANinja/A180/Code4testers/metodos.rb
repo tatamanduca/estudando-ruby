744b2f3628c3d9ddbcd1c68f52f5f953e1e7f45c
tatamanduca <tatamanduca2@gmail.com>
2022-08-07 10:38:57 -0300

    chore: add first steps

---
+++ QANinja/A180/Code4testers/metodos.rb
@@ -0,0 +1,23 @@
+
+# # def imprime_nome(nome)
+# #    puts nome
+# # end
+
+# # imprime_nome("Tassio")
+
+# def soma (valor1, valor2)
+#     #essa função somas os valores desejados por MIM ;D
+# puts valor1 + valor2
+# end
+
+# soma(4, 9)
+
+def pega_soma(valor1, valor2)
+    resultado = valor1 + valor2
+    return resultado
+end
+
+puts pega_soma(10,8)
+
+
+
---
+++ QANinja/A180/Code4testers/tipos.rb
@@ -0,0 +1,10 @@
+nome = "Tamires" 
+idade = 29 
+legal = true
+
+puts nome
+puts nome.class
+puts idade
+puts idade.class
+puts legal
+puts legal.class
